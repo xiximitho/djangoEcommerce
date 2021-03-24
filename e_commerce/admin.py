306929f15b49358572974573720dfd77994379c4
Felipe Mattes <mattesfelipe@email.com>
2021-03-24 07:27:10 -0300

    criado cadastro no admin

--- e_commerce/admin.py
+++ e_commerce/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Produto
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'nome_pagina']
+    prepopulated_fields = {'nome_pagina': ('nome',)}
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'nome_pagina', 'preco',
+                    'disponivel', 'criado', 'atualizado']
+    list_filter = ['disponivel', 'criado', 'atualizado']
+    list_editable = ['preco', 'disponivel']
+    prepopulated_fields = {'nome_pagina': ('nome',)}
