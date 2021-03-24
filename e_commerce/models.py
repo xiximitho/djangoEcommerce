2f33ab33fa9f7c35ba5b4a4c8f1daa73a91ab4ed
Felipe Mattes <mattesfelipe@email.com>
2021-03-24 07:09:35 -0300

    Criado models do db

--- e_commerce/models.py
+++ e_commerce/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200,
+                            db_index=True)
+    nome_pagina = models.SlugField(max_length=200,
+                                   unique=True)
+
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    categoria = models.ForeignKey(Categoria,
+                                  related_name='produtos',
+                                  on_delete=models.CASCADE) # relacionamento 1 pra N (modelo)
+    nome = models.CharField(max_length=200, db_index=True)
+    nome_pagina = models.SlugField(max_length=200, db_index=True)
+    imagem = models.ImageField(upload_to='produtos/%d/%m/%Y', blank=True)
+
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    criado = models.DateTimeField(auto_now_add=True)
+    atualizado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('nome',)
+        index_together = (('id', 'nome_pagina'),)
+
+    def __str__(self):
+        return self.nome
